1b2d1ea5b8d89de293f3fd46ea071b6be2169063
suramya-biswas-git <suramya.biswas@gmail.com>
2025-03-26 19:31:18 +0530

    Second Version

---
+++ mygithubApp/src/mypack/MyDemo.java
@@ -0,0 +1,10 @@
+package mypack;
+
+public class MyDemo {
+
+	public static void main(String[] args) {
+		System.out.println("Hello everyone");
+
+	}
+
+}
--- mygithubApp/src/mypack/Welcome.java
+++ mygithubApp/src/mypack/Welcome.java
@@ -4,6 +4,7 @@ public class Welcome {
 
 	public static void main(String[] args) {
 		System.out.println("Hello Welcome to Github demo");
+		System.out.println("Hi Welcome to Github demo");
 	}
 
 }
